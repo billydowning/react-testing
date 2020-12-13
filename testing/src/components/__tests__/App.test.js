0ef830200aa49f0e64f0cb5818cdc36571896ff2
Billy Downing <wldowning@gmail.com>
2020-12-12 18:22:54 -0700

    before each refactor of tests

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -4,14 +4,16 @@ import App from "components/App";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
-it("shows a comment box", () => {
-  const component = shallow(<App />);
+let component;
 
+beforeEach(() => {
+  component = shallow(<App />);
+});
+
+it("shows a comment box", () => {
   expect(component.find(CommentBox).length).toEqual(1);
 });
 
 it("shows a comment list", () => {
-  const component = shallow(<App />);
-
   expect(component.find(CommentList).length).toEqual(1);
 });
