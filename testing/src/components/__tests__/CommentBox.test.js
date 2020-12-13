b2adfd92db51a52e4030a4b4b3e16b9841e1a9c0
Billy Downing <wldowning@gmail.com>
2020-12-13 14:22:03 -0700

    wrote commentbox submit tests

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -1,1 +1,36 @@
-CommentBox.test.js;
+import React from "react";
+import { mount } from "enzyme";
+
+import CommentBox from "components/CommentBox";
+
+let component;
+
+beforeEach(() => {
+  component = mount(<CommentBox />);
+  component.find("textarea").simulate("change", {
+    target: { value: "new comment" },
+  });
+});
+
+afterEach(() => {
+  component.unmount();
+});
+
+it("has a text area and a button", () => {
+  expect(component.find("textarea").length).toEqual(1);
+  expect(component.find("button").length).toEqual(1);
+});
+
+it("has a text area that users can type in", () => {
+  component.update();
+  expect(component.find("textarea").prop("value")).toEqual("new comment");
+});
+
+it("when form is submitted, text area is emptied", () => {
+  component.update();
+  expect(component.find("textarea").prop("value")).toEqual("new comment");
+  component.find("form").simulate("submit");
+  component.update();
+
+  expect(component.find("textarea").prop("value")).toEqual("");
+});
