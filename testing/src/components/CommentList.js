0cd4c123ecb686f2462ae9a6c30756fed311a1c8
Billy Downing <wldowning@gmail.com>
2020-12-14 14:51:57 -0700

    rendered comment list component

--- testing/src/components/CommentList.js
+++ testing/src/components/CommentList.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const CommentList = () => {
-  return <div>CommentList</div>;
+class CommentList extends React.Component {
+  renderComments() {
+    return this.props.comments.map((comment) => {
+      return <li key={comment}>{comment}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul className="ui list">{this.renderComments()}</ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { comments: state.comments };
 };
 
-export default CommentList;
+export default connect(mapStateToProps)(CommentList);
