a1f3854b3b7e6891d9b7123b31a82e1b69bd8ec8
Billy Downing <wldowning@gmail.com>
2020-12-15 18:48:26 -0700

    added auth

--- testing/src/reducers/index.js
+++ testing/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import commentsReducer from "reducers/commentsReducer";
+import authReducer from "reducers/authReducer";
 
 export default combineReducers({
   comments: commentsReducer,
+  auth: authReducer,
 });
